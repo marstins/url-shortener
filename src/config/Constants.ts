c109e9cb05f7a2992b96ffd24beb5aae9fd1aad1
Matheus Martins <81987419+marstins@users.noreply.github.com>
2022-01-28 15:34:35 -0300

    Update Constants.ts

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
   API_URL: 'http://localhost:5000',
-  MONGO_CONNECTION: 'mongodb+srv://matheus:uE4JefCO7MEd9lxn@url-shortener.dufbh.mongodb.net/url-shortener?retryWrites=true&w=majority'
+  MONGO_CONNECTION: 'mongodb+srv://<username>:<password>@url-shortener.dufbh.mongodb.net/<databaseName>?retryWrites=true&w=majority'
 }
 
